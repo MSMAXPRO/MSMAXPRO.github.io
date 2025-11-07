9c79737ab74d6fdf3209236abf2e1982afb1c63e
Manish singh <princeaary545@gmail.com>
2025-11-07 19:28:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,14 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
             hamburger.addEventListener('click', () => {
                 nav.classList.toggle('nav-active');
                 
-                const navLinks = document.querySelectorAll('.nav-links li');
-                navLinks.forEach((link, index) => {
-                    if (link.style.animation) {
-                        link.style.animation = '';
-                    } else {
-                        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-                    }
-                });
+            
 
                 hamburger.classList.toggle('toggle');
             });
