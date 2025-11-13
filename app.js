a1850a5665a347cff9f729e77c753d4cfb6804fd
Manish singh <princeaary545@gmail.com>
2025-11-13 07:54:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -121,3 +121,10 @@ if (translateBtn) {
         }
     });
 }
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+    .then((reg) => console.log('Service Worker registered', reg))
+    .catch((err) => console.log('Service Worker not registered', err));
+}
+
